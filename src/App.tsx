39357fdab490e6d5ce47a287e97a56b60a5ea923
Maxim Berdnikov <m.radik25@gmail.com>
2022-09-14 22:02:05 +0300

    add useKeyPress hook, install router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Layout } from "Templates/Layout";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  return <Layout />;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
+  );
+};
 
 export default App;
